2bd5b51f0d625e0aebf8d5492f1f7e31fd6d16ca
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2015-03-25 13:53:12 +0000

    Fix tests for readability

--- test/test-readability.js
+++ test/test-readability.js
@@ -1,4 +1,5 @@
 var prettyPrint = require("html").prettyPrint;
+var jsdom = require("jsdom").jsdom;
 var chai = require("chai");
 chai.config.includeStack = true;
 var expect = chai.expect;
@@ -12,7 +13,7 @@ var testPages = require("./bootstrap").getTestPages();
 describe("Test page", function() {
   testPages.forEach(function(testPage) {
     describe(testPage.dir, function() {
-      var doc, result;
+      var doc, jsdomDoc, result, jsdomResult;
 
       var uri = {
         spec: "http://fakehost/test/page.html",
@@ -24,27 +25,36 @@ describe("Test page", function() {
 
       before(function() {
         doc = new JSDOMParser().parse(testPage.source);
+        jsdomDoc = jsdom(testPage.source);
         result = new Readability(uri, doc).parse();
+        jsdomResult = new Readability(uri, jsdomDoc).parse();
       });
 
       it("should return a result object", function() {
         expect(result).to.include.keys("content", "title", "excerpt", "byline");
+        expect(jsdomResult).to.include.keys("content", "title", "excerpt", "byline");
       });
 
-      it("should extract expected content", function() {
+      it("should extract expected content with JSDOMParser", function() {
         expect(testPage.expectedContent).eql(prettyPrint(result.content));
+      });
+      it("should extract expected content with jsdom", function() {
+        expect(testPage.expectedContent).eql(prettyPrint(jsdomResult.content));
       });
 
       it("should extract expected title", function() {
         expect(testPage.expectedMetadata.title).eql(result.title);
+        expect(testPage.expectedMetadata.title).eql(jsdomResult.title);
       });
 
       it("should extract expected byline", function() {
         expect(testPage.expectedMetadata.byline).eql(result.byline);
+        expect(testPage.expectedMetadata.byline).eql(jsdomResult.byline);
       });
 
       it("should extract expected excerpt", function() {
         expect(testPage.expectedMetadata.excerpt).eql(result.excerpt);
+        expect(testPage.expectedMetadata.excerpt).eql(jsdomResult.excerpt);
       });
     });
   });
